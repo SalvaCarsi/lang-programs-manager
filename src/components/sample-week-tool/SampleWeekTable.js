66a0f5a713cbe5e8b55659fcce761338bf66a376
Salva Carsi <salvavcarsi@gmail.com>
2016-12-16 18:53:39 +0100

    simplifying code

--- src/components/sample-week-tool/SampleWeekTable.js
+++ src/components/sample-week-tool/SampleWeekTable.js
@@ -2,7 +2,6 @@ import React from 'react';
 import DAYS from '../../data/days';
 import PERIODS from '../../data/periods';
 import Table from 'rc-table';
-// import MyTable from './MyTable';
 import _ from 'lodash';
 import 'rc-table/assets/index.css';
 
@@ -18,19 +17,17 @@ export default React.createClass({
     return (
       <div>
         <Table columns={this.generateColumns()} data={this.generateRows()} />
-        {/*<MyTable columns={this.generateColumns()} data={this.generateRows()} />*/}
       </div>
     );
   },
   // ================= Helper functions =================
   generateColumns(){
     let columnsHeaderTemplate = {title: '', dataIndex: '', key: '', width: 150};
-    let numberOfDays = this.props.numberOfDays === null ? 7 : this.props.numberOfDays;
-    let firstDay = this.props.firstDay === null ? 0 : this.props.firstDay;
     // building a new DAYS array, where the first element is not Sunday but the one chosen by the user
+    const firstDay = this.props.firstDay;
     let daysRebuild = _.concat([], _.slice(DAYS, firstDay), _.take(DAYS, firstDay));
 
-    let columns = daysRebuild.slice(0, numberOfDays).map(function(day) {
+    let columns = daysRebuild.slice(0, this.props.numberOfDays).map(function(day) {
       let t = _.cloneDeep(columnsHeaderTemplate);
       _.set(t, 'title', day.label.toUpperCase());
       _.set(t, 'dataIndex', day.label.toLowerCase());
@@ -53,13 +50,11 @@ export default React.createClass({
     const afternoonPeriod = PERIODS[1];
     const eveningPeriod = PERIODS[2];
     const fullDayPeriod = PERIODS[3];
-    const numberOfPeriods = this.props.numberOfPeriods === null ? 3 : this.props.numberOfPeriods;
-    const beginningOfPeriod = this.props.beginningOfPeriod === null ? morningPeriod.value : this.props.beginningOfPeriod;
 
     let rows = [];
-    switch (beginningOfPeriod) {
+    switch (this.props.beginningOfPeriod) {
       case morningPeriod.value:
-        switch (numberOfPeriods) {
+        switch (this.props.numberOfPeriods) {
           case 3: rows.unshift(_.set({}, 'period', eveningPeriod.label.toUpperCase())); // falls through
           case 2: rows.unshift(_.set({}, 'period', afternoonPeriod.label.toUpperCase())); // falls through
           case 1: rows.unshift(_.set({}, 'period', morningPeriod.label.toUpperCase())); // falls through
@@ -67,7 +62,7 @@ export default React.createClass({
         }
         break;
       case afternoonPeriod.value:
-        switch (numberOfPeriods) {
+        switch (this.props.numberOfPeriods) {
           case 3:
           case 2: rows.unshift(_.set({}, 'period', eveningPeriod.label.toUpperCase())); // falls through
           case 1: rows.unshift(_.set({}, 'period', afternoonPeriod.label.toUpperCase())); // falls through
@@ -75,7 +70,7 @@ export default React.createClass({
         }
         break;
       case eveningPeriod.value:
-        switch (numberOfPeriods) {
+        switch (this.props.numberOfPeriods) {
           case 3: // falls through
           case 2: // falls through
           case 1: rows.unshift(_.set({}, 'period', eveningPeriod.label.toUpperCase())); // falls through
@@ -83,7 +78,7 @@ export default React.createClass({
         }
         break;
       case fullDayPeriod.value:
-        switch (numberOfPeriods) {
+        switch (this.props.numberOfPeriods) {
           case 3: // falls through
           case 2: // falls through
           case 1: rows.unshift(_.set({}, 'period', fullDayPeriod.label.toUpperCase())); // falls through
